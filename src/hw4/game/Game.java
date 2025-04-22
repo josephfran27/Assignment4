d725fe73193c2804b48661c6e54836a483453baa
Joseph France <jafcxm@umsystem.edu>
2025-04-22 16:40:28 -0500

    Created the different if statements to ensure proper movement in the play method.

--- src/hw4/game/Game.java
+++ src/hw4/game/Game.java
@@ -1,6 +1,8 @@
 package hw4.game;
 
+import hw4.maze.CellComponents;
 import hw4.maze.Grid;
+import hw4.player.Player;
 
 public class Game {
 	
@@ -33,7 +35,41 @@ public class Game {
 		}
 	}
 	
-	
+	public boolean play(Movement movement, Player player) {
+		
+		int currentRowIndex = grid.getRows().indexOf(player.getCurrentRow());
+		int currentCellIndex = player.getCurrentRow().getCells().indexOf(player.getCurrentCell());
+		
+		if(movement == Movement.UP) {
+			//check if player is not on top row already, check if upper part of current  cell is a wall, check if lower part of above cell is a wall
+			if (currentRowIndex > 0 && player.getCurrentCell().getUp() != CellComponents.WALL 
+					&& grid.getRows().get(currentRowIndex - 1).getCells().get(currentCellIndex).getDown() != CellComponents.WALL) {
+				return true;
+			}
+		}
+		else if (movement == Movement.DOWN) {
+			//check if player is not at the bottom row, check if the lower part of the current cell is a wall, check if the upper part of the lower cell is a wall
+			if (currentRowIndex < grid.getRows().size() - 1 && player.getCurrentCell().getDown() != CellComponents.WALL 
+					&& grid.getRows().get(currentRowIndex + 1).getCells().get(currentCellIndex).getUp() != CellComponents.WALL) {
+				return true;
+			}
+		}
+		else if (movement == Movement.LEFT) {
+			//check if player is not at the left most row, check if the left part of the current cell is a wall, check if the right side of the left cell is a wall
+			if (currentCellIndex > 0 && player.getCurrentCell().getLeft() != CellComponents.WALL
+					&& player.getCurrentRow().getCells().get(currentCellIndex - 1).getRight() != CellComponents.WALL) {
+				return true;
+			}
+		}
+		else if (movement == Movement.RIGHT) {
+			//check if player is not at the right most row, check if the right part of the current cell is a wall, check if the left side of the right cell is a wall
+			if (currentCellIndex < player.getCurrentRow().getCells().size() - 1 && player.getCurrentCell().getRight() != CellComponents.WALL
+					&& player.getCurrentRow().getCells().get(currentCellIndex + 1).getLeft() != CellComponents.WALL) {
+				return true;
+			}
+		}
+		return false;
+	}
 	
 
 }
